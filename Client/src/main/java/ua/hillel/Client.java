0c26b78e0f7c7af1ab3bb63803598d343906eeff
Dupliy Vitaliy <gidrohlorid9@gmail.com>
2023-08-23 22:55:46 +0300

    commit1

---
+++ Client/src/main/java/ua/hillel/Client.java
@@ -0,0 +1,49 @@
+package ua.hillel;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class Client {
+
+	public static void main(String[] args) {
+		try (Socket socket = new Socket("127.0.0.1", 8080); Scanner scanner = new Scanner(System.in)) {
+
+						
+			Thread fromServerThread = new Thread(new Runnable() {
+
+				@Override
+				public void run() {
+
+					while (true) {
+						try {
+							System.out.println(new DataInputStream(socket.getInputStream()).readUTF());
+						} catch (IOException e) {
+							throw new RuntimeException(e);
+						}
+					}
+				}
+			});
+
+			fromServerThread.setDaemon(true);
+			fromServerThread.start();
+
+			while (true) {
+				String text = scanner.nextLine();
+
+				if (text.equals("exit")) {
+					break;
+				}
+				new DataOutputStream(socket.getOutputStream()).writeUTF(text);
+			}
+
+		} catch (IOException e) {
+
+			e.printStackTrace();
+		}
+
+	}
+
+}
